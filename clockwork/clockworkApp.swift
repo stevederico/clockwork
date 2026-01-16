11bde651b841b393106229c74a524dfadfe9fbf4
Steve Derico <steve@bixbyapps.com>
2026-01-16 14:24:16 -0800

    Initial Commit

---
+++ clockwork/clockworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockworkApp.swift
+//  clockwork
+//
+//  Created by Steve Derico on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct clockworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
